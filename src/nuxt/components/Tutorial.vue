6c88c8cf32da70bc37fbc0d521f1f3bd48398407
9inelemons <9inelemons@gmail.com>
2022-07-15 18:30:45 +0600

    Added NuxtJS container

---
+++ src/nuxt/components/Tutorial.vue
@@ -0,0 +1,20 @@
+<!-- Please remove this file from your project -->
+<template>
+  <h1>{{ data }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+
+  data () {
+    return {
+      data: '',
+    }
+  },
+  async fetch() {
+    const res = await this.$axios.get('/test')
+    this.data = res.data.message
+  },
+}
+</script>
